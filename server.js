91ff5750a9388b61a481d31e3b7b1b319409f568
Bryce Hasshaw <bhasshaw@gmail.com>
2018-11-18 11:29:07 -0800

    test

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'CLIENT_ORIGIN');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
