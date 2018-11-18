7593770e97971854312035a892e014103fec8534
Bryce Hasshaw <bhasshaw@gmail.com>
2018-11-18 10:59:47 -0800

    test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/react-capstone-api';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/react-capstone-api-test';
 exports.PORT = process.env.PORT || 8080;
